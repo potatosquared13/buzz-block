94e647a7410968de9a1344b194e33d2e927a67c1
Daniel Eldrick Villanueva <danielvillanueva@Daniels-MacBook-Pro.local>
2019-05-10 03:01:29 +0800

    made the template. added two way data binding. now need to find a way to connect with flask so it can communicate with server.

---
+++ Website/buzz-form/src/app/person.model.ts
@@ -0,0 +1,10 @@
+export class Person {
+	constructor(
+		public id: number,
+		public fullName: string,
+		public age: number,	
+		public email: string,
+		public address: string,
+		public phoneNumber: string
+	) { }
+}
