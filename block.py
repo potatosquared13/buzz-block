2a8a21fd0767477e4b47ab7687e62c2af702f4e9
jia <dftst0@gmail.com>
2019-07-30 22:26:53 +0800

    remove mining, move pending transactions to tracker

--- block.py
+++ block.py
@@ -1,12 +1,11 @@
 import helpers
 from copy import deepcopy
 from datetime import datetime
-from transaction import *
+from transaction import Transaction
 
 class Block:
     def __init__(self, index, previous_hash, transactions):
         self.index = index
-        self.nonce = 0
         self.previous_hash = previous_hash
         self.timestamp = datetime.now().isoformat()
         self.transactions = transactions
@@ -18,14 +17,12 @@ class Block:
     @property
     def hash(self):
         tmp_block = deepcopy(self)
-        del tmp_block.nonce
-        return helpers.sha256(helpers.jsonify(tmp_block)).hexdigest()
+        return helpers.sha256(helpers.jsonify(tmp_block))
 
 
 class Blockchain:
     def __init__(self):
         self.blocks = []
-        self.pending_transactions = []
 
     # create an empty block for the first block
     def genesis(self):
@@ -34,31 +31,31 @@ class Blockchain:
             previous_hash="0000000000000000000000000000000000000000000000000000000000000000",
             transactions=[]
         )
-        block.nonce = self.mine(0, block)
+        # block.nonce = self.mine(0, block)
         self.blocks.append(block)
 
     # adds all verified pending transactions to block
     # and creates a new one
     # NOTE: transactions that have not been verified are
     # REMOVED COMPLETELY, and don't stay in pending_transactions
-    def write_block(self):
-        previous_nonce = self.last_block.nonce
+    def write_block(self, new_transactions):
+        # previous_nonce = self.last_block.nonce
         previous_hash = self.last_block.hash
         block = Block(
             index=len(self.blocks),
             previous_hash=previous_hash,
-            transactions=deepcopy(self.pending_transactions)
+            transactions=new_transactions
         )
-        self.pending_transactions = []
-        block.nonce = self.mine(previous_nonce, block)
+        # self.pending_transactions = []
+        # block.nonce = self.mine(previous_nonce, block)
         self.blocks.append(block)
 
-    def add_transaction(self, transaction):
-        self.pending_transactions.append(transaction)
+    # def add_transaction(self, transaction):
+    #     self.pending_transactions.append(transaction)
 
-    @property
-    def transactions(self):
-        return helpers.jsonify(self.pending_transactions)
+    # @property
+    # def transactions(self):
+    #     return helpers.jsonify(self.pending_transactions)
 
     @property
     def json(self):
@@ -68,12 +65,13 @@ class Blockchain:
     def last_block(self):
         return self.blocks[-1]
 
-    @staticmethod
-    def mine(previous_nonce, block):
-        nonce = previous_nonce
-        hash = block.hash
-        while True:
-            digest = helpers.sha256(str(nonce) + str(hash)).hexdigest()
-            if (digest.startswith('000')):
-                return nonce
-            nonce += 1
+    # @staticmethod
+    # def mine(previous_nonce, block):
+    #     nonce = previous_nonce
+    #     hash = block.hash
+    #     while True:
+    #         digest = helpers.sha256(str(nonce) + hash)
+    #         if (digest.startswith('000')):
+    #             return nonce
+    #         nonce += 1
+
