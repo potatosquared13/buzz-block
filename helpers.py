4b8a29a537f1a59cff19d8abb8f9d01e32e828cc
jia <dftst0@gmail.com>
2019-07-31 20:19:03 +0800

    switch to logging from own implementation

--- helpers.py
+++ helpers.py
@@ -1,9 +1,9 @@
 import json
+
 from datetime import datetime
 from binascii import hexlify
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
-# from Crypto.Hash import SHA256
 
 
 # returns utf8-encoded sha256 hash of message
@@ -16,9 +16,4 @@ def sha256(message):
 # returns obj as a json object
 def jsonify(obj):
     return json.dumps(obj, default=lambda o: o.__dict__, indent=4)
-
-# prints message with timestamp
-def log(msg):
-    print("[" + datetime.now().isoformat() + "] " , end='', flush=True)
-    print(msg)
 
