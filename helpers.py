f97a1239a8e218ec72dabf6f996828d4fede1fce
John Edcel Recio <edcelfr@gmail.com>
2019-10-22 17:00:09 +0800

    add function for formatting numbers with commas and two decimal values

--- helpers.py
+++ helpers.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from binascii import hexlify
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
+from math import trunc
 
 # returns utf8-encoded sha256 hash of message
 def sha256(message):
@@ -18,3 +19,14 @@ def sha256(message):
 def jsonify(obj):
     return json.dumps(obj, default=lambda o: o.__dict__, separators=(',',':'), indent=None, sort_keys=True)
 
+def format_number(number):
+    value_past_decimal = number - trunc(number)
+    number_sequence = str(trunc(number))
+    new_number_sequence = ''
+    reverse_sequence = number_sequence[::-1]
+    for i in range(len(reverse_sequence)):
+        if (((i + 1) % 3) == 0) and (i != len(reverse_sequence) - 1):
+            new_number_sequence = ',' + reverse_sequence[i] + new_number_sequence
+        else:
+            new_number_sequence = reverse_sequence[i] + new_number_sequence
+    return new_number_sequence +'.{0:.2f}'.format(value_past_decimal)[2:]
